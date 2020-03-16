7556097c757243263d8dbea5aab3d8277a1632b3
Ahmed Amr <ahmedbonumstelio@gmail.com>
2020-03-16 02:46:46 +0200

    dp table working, but not correctly

--- core.py
+++ core.py
@@ -5,69 +5,50 @@ class Core:
     def __init__(self,image):
         self.image = image
     
-    def getSeam(self):
-        ''' return the seam with the lowest energy '''
-        memo = {}
-        mi = vars.inf,[]
-        for i in range(self.image.w):
-            po,li = self.getSeamof((0,i),memo)
-            if po < mi[0]:
-                mi = po,li
-            print(i,po)
-        return po,li
-            
+    def DP(self):
+        img   = self.image
+        GE    = self.image.getEnergy
+        VA    = self.image.valid
+        table = [[(0,0) for i in range(img.w)] for i in range(img.h)]
+        dh    = (-1,0,1)
+        for h in range(img.w):
+            for v in range(1,img.h):
+                mi = vars.inf
+                for sh in dh:
+                    nh = h+sh
+                    if VA(v-1,nh):
+                        t = GE(v-1,nh)
+                        if t < mi:
+                            mi = t
+                            table[v][h] = mi,sh
+        return table
 
-    def getSeamof(self,pixel,memo):
-        ''' return the seam with the lowest energy starting at `pixel` '''
-        dh  = [-1,0,1]
-        # print(pixel)
-        img = self.image
-        track = [0 for i in range(img.h)]
-        hig = img.h
-        def calc(pixel):
-            print(pixel)
-            ret = memo.get(pixel,None)
-            if ret:
-                return ret
-            pv = pixel[0]
-            if pv == img.h:
-                return 0
-            if not img.valid(pixel[0],pixel[1]):
-                return 1<<32
-            ret = image.getEnergy(pv,pixel[1])
-            mi = vars.inf
-            for sh in dh:
-                ph = pixel[1]+sh
-                tmp = calc((pv+1,ph))
-                if tmp < mi:
-                    mi = tmp
-                    track[pv]=sh
-            ret += mi
-            memo[pixel]  = ret
-            return ret 
-        return calc(pixel),track
-                    
+    def buildseam(self,table):
+        h = 0
+        mx = 0
+        hi = self.image.h
+        for e,i in enumerate(table[hi-1]):
+            if i[0] > mx:
+                h = e
+        li = []
+        for v in range(hi-1,-1,-1):
+            li.append((v,h))
+            h+= table[v][h][1]
+        return li
 
 
-os.sys.setrecursionlimit(100000000)
-
-image= Imager(vars.org)
-def test_getseamof():
+image= Imager(vars.muf)
+def test_dp():
     ob = Core(image)
-    print('mt')
-    return ob.getSeamof((0,100),{})
-def test_getseam():
-    ob = Core(image)
-    return ob.getSeam()
+    print('dp...')
+    return ob.buildseam(ob.DP())
+
+x = test_dp()
 
 def test_colorseam(x):
-    pv,ph = 0,0
-    for i in x:
-        ph+=i
-        image.setpixel(pv,ph,[255,255,255])
-        pv+=1
+    for a,b in x:
+        image.setpixel(a,b,[255,255,255])
     image.close('imgs/coloroneseam.jpeg')
+test_colorseam(x)
 
-x = test_getseamof()
-test_colorseam(x[1])
 
