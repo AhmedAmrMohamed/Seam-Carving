43fcdc7f558f262122f982eec9754e6e420160c4
Ahmed Amr <ahmedbonumstelio@gmail.com>
2020-03-16 22:32:02 +0200

    Imager.buildEnergy is working as expected

--- imager.py
+++ imager.py
@@ -1,131 +1,116 @@
-import numpy as np
 from PIL import Image
 
 class Imager:
     ''' handles the all the related image proceesing stuff'''
-    def __init__(self,orgPath):
-        self.orgPath  = orgPath
-        self.img      = self.open()
-        self.h,self.w = len(self.img),len(self.img[0])
+    def __init__(self,orgPath,destPath):
+        self.orgPath    = orgPath
+        self.image      = Image.open(orgPath)
+        self.hs,self.vs = self.image.size
+        self.table      = self.totable()
+        self.entab      = self.buildEnergy()
+        self.close(self.entab,destPath)
 
-    def open(self):
-        '''open the image into an numpy array'''
-        image = Image.open(self.orgPath)
-        print(image.size)
-        data  = np.array(image)
-        return data
+    def totable(self):
+        ''' convert the pick into a table of pixels '''
+        print('converting to table...')
+        img = self.image
+        table = [[ img.getpixel((j,i))for i in range(self.vs)] for j in range(self.hs)]
+        print('converted.')
+        return table
 
-    def close(self,path):
+    def close(self,table,path):
         ''' save the img in path '''
-        Image.fromarray(self.img).save(path)
-    
-    def valid (self,pixelv,pixelh):
-        validv = pixelv >=0 and pixelv < self.h
-        validh = pixelh >=0 and pixelh < self.w
-        return validv and validh
-    
-    def setpixel(self,pixelv,pixelh,rgb):
-        ''' set the value of a pixel as [r,g,b] '''
-        # np.put(self.img,[pixelx,pixely],[r,g,b])
-        # np.put(self.img,[pixelx,pixely],[r,g,b])
-        self.img[pixelv,pixelh,0] = rgb[0]
-        self.img[pixelv,pixelh,1] = rgb[1]
-        self.img[pixelv,pixelh,2] = rgb[2]
+        print('closing...')
+        copy = self.image.copy()
+        for i in range(self.hs-1):
+            for j in range(self.vs-1):
+                try:
+                    pixel = table[i][j]
+                except Exception as ex:
+                    print(f'{ex},{i},{j},{self.hs},{self.vs}')
+                copy.putpixel((i,j),pixel)
+        copy.save(path)
 
-    def getpixel(self,pixelv,pixelh):
-        ''' get the value of a pixel '''
-        if not self.valid(pixelv,pixelh):
+    def validPixel(self,pixel):
+        a = pixel[0] >= 0 and pixel[0] < self.hs
+        b = pixel[1] >= 0 and pixel[1] < self.vs
+        return a and b
+    
+    def getPixel(self,pixel):
+        if not self.validPixel(pixel):
             return (0,0,0)
-        ret =  self.img[pixelv][pixelh]
-        return tuple(map(int,(ret[0],ret[1],ret[2])))
-    
-    def removeSeam(self,seam):
-        ''' 
-        remove the the pixels refered to by seam
-        by shifting adjacent pixels into their place
-        '''
-        width = self.w
-        gp    = self.getpixel
-        sp    = self.setpixel
-        for v,h in seam:
-            for i in range(h+1,width):
-                sp(v,i-1,gp(v,i-1))
+        return self.table[pixel[0]][pixel[1]]
 
-
-    def getEnergy(self,pixelv,pixelh):
+    def getPixelEnergy(self,pixel):
         dv = [ 0, 0,-1, 1]
         dh = [-1, 1, 0, 0]
-        le,ri,do,up = ((pixelv+a,pixelh+b) for a,b in zip(dv,dh))
-        le = self.getpixel(le[0],le[1])
-        ri = self.getpixel(ri[0],ri[1])
-        do = self.getpixel(do[0],do[1])
-        up = self.getpixel(up[0],up[1])
+        le,ri,do,up = (self.getPixel((pixel[0]+a,pixel[1]+b)) for a,b in zip(dv,dh))
 
         dh = ((a-b)**2 for a,b in zip(le,ri))
         dv = ((a-b)**2 for a,b in zip(up,do))
 
         return sum(dh)+sum(dv)
+    def buildEnergy(self):
+        table = [[ self.getPixelEnergy((j,i))for i in range(self.vs)] for j in range(self.hs)]
+        return table
+        
+        
+    
+    # def valid (self,pixelv,pixelh):
+        # validv = pixelv >=0 and pixelv < self.h
+        # validh = pixelh >=0 and pixelh < self.w
+        # return validv and validh
+    
+    # def setpixel(self,pixelv,pixelh,rgb):
+        # ''' set the value of a pixel as [r,g,b] '''
+        # self.img[pixelv,pixelh,0] = rgb[0]
+        # self.img[pixelv,pixelh,1] = rgb[1]
+        # self.img[pixelv,pixelh,2] = rgb[2]
+
+    # def getpixel(self,pixelv,pixelh):
+        # ''' get the value of a pixel '''
+        # if not self.valid(pixelv,pixelh):
+            # return (0,0,0)
+        # ret =  self.img[pixelv][pixelh]
+        # return tuple(map(int,(ret[0],ret[1],ret[2])))
+    
+    # def removeSeam(self,seam):
+        # ''' 
+        # remove the the pixels refered to by seam
+        # by shifting adjacent pixels into their place
+        # '''
+        # width = self.w
+        # gp    = self.getpixel
+        # sp    = self.setpixel
+        # for v,h in seam:
+            # for i in range(h+1,width):
+                # sp(v,i-1,gp(v,i-1))
+
+
+    # def getEnergy(self,pixelv,pixelh):
+        # dv = [ 0, 0,-1, 1]
+        # dh = [-1, 1, 0, 0]
+        # le,ri,do,up = ((pixelv+a,pixelh+b) for a,b in zip(dv,dh))
+        # le = self.getpixel(le[0],le[1])
+        # ri = self.getpixel(ri[0],ri[1])
+        # do = self.getpixel(do[0],do[1])
+        # up = self.getpixel(up[0],up[1])
+
+        # dh = ((a-b)**2 for a,b in zip(le,ri))
+        # dv = ((a-b)**2 for a,b in zip(up,do))
+
+        # return sum(dh)+sum(dv)
         
 
 
 from vars import *
-def test_open():
-    ob = Imager(new)
-    print(len(ob.img[0]))
-    print(ob.w,ob.h)
-    print(ob.img[0][0])
-    return ob
 
+# img = Imager(org)
+def test_toarray():
+    return img.toarray()
 def test_close():
-    ob = Imager('imgs/surforg.jpeg')
-    ob.close('imgs/surfnew.jpeg')
+    img.close(path+'testclose.jpg')
+def test_energy():
+    img = Imager(org,'imgs/ene.jpeg')
 
-def test_setpixel():
-    ob=Imager(org)
-    print('org' , ob.getpixel(1,1) )
-    ob.setpixel(1,1,255,255,255)
-    print('new' , ob.getpixel(1,1) )
-
-def test_setpixel2():
-    ob = Imager(low)
-    print(ob.h)
-    for i in range(ob.w):
-            ob.setpixel(0,i,[0,0,0])
-    for i in range(ob.h):
-            ob.setpixel(i,0,[255,255,255])
-    ob.close(new)
-def test_removeseam():
-    ob = Imager(org)
-    seam = [400 for i in range(ob.h-2)]
-    for i in range(50):
-        print(i)
-        ob.removeSeam(seam)
-    ob.close('imgs/1.jpeg')
-
-def test_calcenergy():
-    ob = Imager(org)
-    for i in range(ob.w-1):
-        for j in range(ob.h-1):
-            print(ob.calcEnergy(j,i))
-def test_valid():
-    ob = Imager(low)
-    assert ob.valid(0,0)
-    assert not ob.valid(-1,0)
-    assert not ob.valid(0,-1)
-
-def test_getEnergy():
-    ob = Imager(muf)
-    sv = Imager('imgs/en.jpeg')
-    for i in range(ob.w):
-        for j in range(ob.h):
-            sv.setpixel(j,i,[ob.getEnergy(j,i),0,0])
-    sv.close('imgs/en.jpeg')
-
-# test_open()
-#test_close()
-# test_setpixel2()
-#test_removeseam()
-# test_calcenergy()
-test_getEnergy()
-# test_valid()
-
+x=test_energy() 
